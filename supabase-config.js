2ffb78c36ae4f971b89e1bb7baae42ccfe05855b
Shopbrain-dot <businessmuhammed41@gmail.com>
2026-08-22 23:38:07 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://eaefuwdtscpjkzsnctrr.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_PUBLISHABLE_KEY";
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
