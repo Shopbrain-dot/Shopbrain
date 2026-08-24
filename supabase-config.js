3c33da95143fc02e6f04a2958f9a7917a2b200c3
Shopbrain-dot <businessmuhammed41@gmail.com>
2026-08-24 15:15:24 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,10 +1,9 @@
-const SUPABASE_URL =
-  "https://wjnugjgrtdswzwldyxay.supabase.co";
+const SUPABASE_URL = "https://wjnugjgrtdswzwldyxay.supabase.co";
 
-const SUPABASE_ANON_KEY =
+const SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_9Og3b_Z6fz9hBrNEYBvQ_w_iym-v3rl";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  SUPABASE_PUBLISHABLE_KEY
 );
